93203524b5bbfcba697c0c5fb57fe30cba774659
Daniel Di Giovanni <dannyjdigio@gmail.com>
2024-09-13 10:15:12 -0400

    Added a setup file to cbwc tool to make it executable from any location (it was previously necessary to do this manually).

---
+++ cbwc/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Sets up the cbwc tool so that it can be executed from any location.
+
+# Copy cbwc to /usr/local/bin/
+sudo cp cbwc/cbwc /usr/local/bin
+
+# Make cbwc executable
+sudo chmod +x /usr/local/bin/cbwc
+
