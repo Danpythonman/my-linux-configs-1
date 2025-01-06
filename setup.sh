0a81694e4069b20d70d383d4a4568f9c06dde418
Daniel Di Giovanni <dannyjdigio@gmail.com>
2025-01-06 14:43:08 -0500

    tasksync tool

--- setup.sh
+++ setup.sh
@@ -10,3 +10,6 @@ bash vim/Vimh/setup.sh
 
 # Setup lines tool
 bash lines/setup.sh
+
+# Setup tasksync tool
+bash task/setup.sh
---
+++ task/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Sets up the tasksync tool so that it can be executed from any location.
+
+# Copy tasksync to /usr/local/bin/
+sudo cp task/tasksync /usr/local/bin
+
+# Make tasksync executable
+sudo chmod +x /usr/local/bin/tasksync
+
