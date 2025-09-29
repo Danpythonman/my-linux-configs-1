e2be1097b2a2015cb249789896dd5e06cb1bba0d
Daniel Di Giovanni <dannyjdigio@gmail.com>
2025-09-29 02:13:17 -0400

    Added proper permissions to write to desktop files for dolphin and corrected type in exec command.

--- dolphin/setup.sh
+++ dolphin/setup.sh
@@ -6,7 +6,9 @@ sudo apt install dolphin qt5-style-kvantum qt5ct breeze-icon-theme
 echo 'export QT_QPA_PLATFORMTHEME=qt5ct' >> ~/.profile
 
 icon_path=$(realpath dolphin/Dolphin_icon.png)
-sed -i "s|^Icon=.*|Icon=$icon_path|" /usr/share/applications/org.kde.dolphin.desktop
+sudo sed -i "s|^Icon=.*|Icon=$icon_path|" /usr/share/applications/org.kde.dolphin.desktop
 
 qss_path=$(realpath dolphin/dolphin.qss)
-sed -i "s|^Exec=.*|Exec=dolhpin %u -stylesheet $qss_path|" /usr/share/applications/org.kde.dolphin.desktop
+sudo sed -i "s|^Exec=.*|Exec=dolphin %u -stylesheet $qss_path|" /usr/share/applications/org.kde.dolphin.desktop
+
+echo 'export QT_QPA_PLATFORMTHEME=qt5ct' >> ~/.profile
