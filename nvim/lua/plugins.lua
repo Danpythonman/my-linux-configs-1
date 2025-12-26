d1535cf2b705cb74e6e197bd47254c0ab4e82fac
Daniel Di Giovanni <dannyjdigio@gmail.com>
2025-12-26 02:33:31 -0500

    Small tree-sitter fix

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -112,7 +112,7 @@ require("lazy").setup({
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
             lspconfig.lua_ls.setup({ capabilities = capabilities })
             lspconfig.pyright.setup({ capabilities = capabilities })
-            lspconfig.ts_ls.setup({ capabilities = capabilities })
+            lspconfig.tsserver.setup({ capabilities = capabilities })
         end,
     },
 
